e4b6fb833c0602f8ead5aed0e25fa24199d01b12
Torumoy Ghoshal <toru@MacBook-Pro.local>
2020-05-21 13:37:44 -0500

    fixed sliding window file for 1D

--- sliding_window.py
+++ sliding_window.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 def create_patches_add_label(image_sample, image_sample_label):
+    data_patches = [[]]
     greyIm = np.true_divide(image_sample, 255)
 
     S = greyIm.shape
@@ -21,21 +22,31 @@ def create_patches_add_label(image_sample, image_sample_label):
             temp_array.append(i)
         temp_array.append(image_sample_label)
         data_patches.append(temp_array)
+        
+    return(data_patches)
 
 
-data = pd.read_csv('train_randomized.csv', header=None)
-features = data.columns[1:]
-label = data.columns[0]
-data_features = data[features]
-data_label = data[label]
-data_features_np = data_features.to_numpy()
-data_label_np = data_label.to_numpy()
+def main(filename_prefix):
+    input_filename = filename_prefix + ".csv"
+    output_filename = filename_prefix + "_patches.csv"
 
-data_patches = []
+    data = pd.read_csv(input_filename, header=None)
+    features = data.columns[1:]
+    label = data.columns[0]
+    data_features = data[features]
+    data_label = data[label]
+    data_features_np = data_features.to_numpy()
+    data_label_np = data_label.to_numpy()
 
-for i in range(len(data_features_np)):
-    create_patches_add_label(data_features_np[i], data_label_np[i])
+    data_patches = []
 
-data_patches_pd = pd.DataFrame(data_patches)
-data_patches_pd.to_csv('train_randomized_patches.csv',
-                       encoding='utf-8', index=False, header=None)
+    for i in range(len(data_features_np)):
+        data_patches.append(create_patches_add_label(data_features_np[i], 
+                                                            data_label_np[i]))
+
+    data_patches_pd = pd.DataFrame(data_patches)
+    data_patches_pd.to_csv(output_filename,
+                        encoding='utf-8', index=False, header=None)
+
+if __name__ == "__main__":
+    main('train_randomized')
